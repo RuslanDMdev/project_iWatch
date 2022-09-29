ae0baf49ab975395d3e2ec03bb3c2cb6cd94f1ad
RuslanDMdev <deelcrafting@gmail.com>
2022-09-29 14:15:52 +0300

    com1

---
+++ project_iWatch WatchKit AppUITests/project_iWatch_WatchKit_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  project_iWatch_WatchKit_AppUITestsLaunchTests.swift
+//  project_iWatch WatchKit AppUITests
+//
+//  Created by Ruslan Dalgatov on 29.09.2022.
+//
+
+import XCTest
+
+class project_iWatch_WatchKit_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ project_iWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project_iWatch
+//
+//  Created by Ruslan Dalgatov on 29.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
