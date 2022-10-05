f15130f02e60f4dc35b877614a73e8dea69a0882
RuslanDMdev <91013665+RuslanDMdev@users.noreply.github.com>
2022-10-05 18:02:53 +0300

    add icons

--- project_iWatch WatchKit Extension/InterfaceController.swift
+++ project_iWatch WatchKit Extension/InterfaceController.swift
@@ -8,38 +8,41 @@
 import WatchKit
 import Foundation
 import UIKit
+import MediaPlayer
 
 
 class InterfaceController: WKInterfaceController {
 
-
+    var player: AVPlayer!
     @IBOutlet weak var button: WKInterfaceButton!
     @IBOutlet weak var coinName: WKInterfaceLabel!
-    
     var coinImagesArray: [UIImage] = []
-    
     @IBOutlet weak var image: WKInterfaceImage!
-    
-
-        
-    
-    
-//    createCoinImagesArray(count: 76, name: "Coin")
-    
     @IBAction func buttonTapped() {
-        
+  
+        image.setImageNamed("animation_images")
+//        coinAnimation()
+    }
+    
+    func coinAnimation(){
+    
         let status = Bool.random()
         
         if status {
             image.setImageNamed("head")
             coinName.setText("Орёл")
+            player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "coin2", ofType: "mp3")!))
+            player.play()
         } else {
             image.setImageNamed("tail")
             coinName.setText("Решка")
+            player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "coin2", ofType: "mp3")!))
+            player.play()
         }
         
-
     }
 
+
+    
 }
 
